82f4bcf1b5a49084280b00cef93dadb1eb5ed1a8
Brecht Hoflack <brh@melexis.com>
2014-07-28 12:06:55 +0200

    Add protractor tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,41 +2,40 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/getting-started.md */
 
-describe('my app', function() {
+describe('CMIS dashboard', function() {
 
-  browser.get('index.html');
-
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+  beforeEach(function() {
+      browser.get('index.html');
   });
 
+  it('Should filter the cis as the user types in the search box', function () {
+      var cis = element.all(by.repeater('ci in cis'));
+      var query = element(by.model('query'));
+      
+      expect(cis.count()).toBe(3);
 
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view1');
-    });
-
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
-    });
-
+      query.sendKeys('httpd');
+      expect(cis.count()).toBe(1);
   });
 
+  it('Should allow filtering using the hostname', function() {
+      var cis = element.all(by.repeater('ci in cis'));
+      var query = element(by.model('query'));
+      
+      query.sendKeys('dlog1.sensors.elex.be');
+      expect(cis.count()).toBe(1);
+  });
 
-  describe('view2', function() {
+  it('Should contain a graph for every measurement', function() {
+      var query = element(by.model('query'));
 
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
+      query.sendKeys('dlog');
+      var charts = element.all(by.css('.chart'));
+      expect(charts.count()).toBe(5);
 
+      charts.each(function(chart) {
+	  expect(chart.getTagName()).toBe("canvas");
 
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
+      });
   });
 });
